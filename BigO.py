2d443b9377880a421935818a6c0eeed89543a1d2
ArmanKamal <53625839+ArmanKamal@users.noreply.github.com>
2020-03-07 23:13:58 +0600

    BigO Examples

---
+++ BigO.py
@@ -0,0 +1,27 @@
+### Big O(N) example ###
+
+### If element and operation increase in a same rate then it is called linear time ###
+
+# def findingNemo(array):
+#     for x in array:
+#         if(x == 'nemo') :
+#             print('Nemo Found')
+#
+#
+#
+# array = ['nemo','nemo','nemo','nemo','nemo','nemo','nemo','nemo','nemo','nemo',]
+#
+# findingNemo(array)
+
+### Big O(1) example ###
+
+### This is a constant time example. Even if you increase the elements the operation remains same.
+#
+# def findTwoBoxes(boxes):
+#     for i in range(1):
+#         print(boxes[0])
+#         print(boxes[1])
+#
+# boxes = [10,20,30,40,50,60,70,80,90,100]
+#
+# findTwoBoxes(boxes)
